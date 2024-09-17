95b6298304f1339ef81a2fe27254d718a5f68281
Douglas <douglasbarra11@hotmail.com>
2024-09-17 20:45:37 -0300

    fazendo o swagger funcionar

--- src/receptor.js
+++ src/receptor.js
@@ -1,18 +1,13 @@
-const express = require('express')
-const receptor = express()
-const controllers = require('./controllers/roteador.js')
+const express = require('express');
+const receptor = express();
+const controllers = require('./controllers/roteador.js');
 const swaggerDocs = require('./swagger');
 
-receptor.use(express.json())
+receptor.use(express.json());
 
-receptor.listen(8080, function () { 
+// Iniciar o servidor apenas uma vez
+receptor.listen(8080, function () {
     console.log('Aplicação executando na porta 8080!');
-})
-
-
-swaggerDocs(app);
-    
-receptor.listen(8080, () => {
-    console.log('Aplicação rodando na porta 8080');
 });
-    
+
+swaggerDocs(receptor);
