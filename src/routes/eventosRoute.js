fb4115f7814c5a1e553ebdf94fd5a247a31e73bf
MateusSdeMatos <mateus.sdematos@gmail.com>
2024-10-05 22:48:00 -0300

    Correções

--- src/routes/eventosRoute.js
+++ src/routes/eventosRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router()
-const EventosController = require('../controllers/eventoController');
+const EventosController = require('../controllers/eventosController');
 
 /**
  * @swagger
