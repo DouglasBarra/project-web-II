67d064b9a7c23f78a1754f08cb21314e8021d531
MateusSdeMatos <mateus.sdematos@gmail.com>
2024-10-05 22:44:40 -0300

    Inclusão de eventos no app

--- src/app.js
+++ src/app.js
@@ -7,12 +7,14 @@ const profissionaisRoute = require('./routes/profissionaisRoute')
 const professoresRoute = require('./routes/professoresRoute')
 const alunosRoute = require('./routes/alunosRoute')
 const agendamentosRoute = require('./routes/agendamentosRoute')
+const eventosRoute = require('./routes/eventosRoute')
 
 app.use(express.json());
 app.use('/api', profissionaisRoute);
 app.use('/api', professoresRoute)
 app.use('/api', alunosRoute)
 app.use('/api', agendamentosRoute)
+app.use('api', eventosRoute)
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
