16d40cef47d7b654b32d81ef29b3d069ddea1732
MateusSdeMatos <mateus.sdematos@gmail.com>
2024-10-06 21:35:18 -0300

    Crição do db, controller e model dos Usuários

---
+++ src/controllers/usuarios.Controller.js
@@ -0,0 +1,52 @@
+const UsuáriosService = require('../services/usuariosService.js');
+
+const UsuáriosController = {
+    getAll: (req, res) => {
+        try {
+            const usuarios = UsuáriosServices.getAll();
+            res.json(usuarios);
+        }   catch (error) {
+            res.status(500).json({ error: 'Erro ao buscar usuários'});   
+        }
+    },
+
+    create: (req, res) => {
+        try {
+            const novoUsuário = req.usuario;
+            const usuarioCriado = UsuáriossService.create(novoUsuário);
+            res.status(201).json(usuarioriado);
+        }   catch (error) {
+            res.status(500).json({ error: 'Erro ao criar usuário.'});
+        }
+    },
+
+    update: (req, res) => {
+        try {
+            const id = req.params.id;
+            const dadosAtualizados = req.body;
+            const usuarioAtualizado = UsuáriosService.update(id, dadosAtualizados);
+        } catch (error) {
+            if (error.message === 'Usuário não encontrado') {
+                res.status(404).json({ error: 'Usuário não encontrado.'});
+            } else {
+                res.status(500).json({ error: 'Erro ao atualizar usuário.'});
+            }
+        }
+    },
+
+    delete: (req, res) => {
+        try {
+            const id = req.params.id;
+            const usuarioRemovido = UsuáriosService.delete(id);
+            res.json({ message: 'Usuário removido com sucesso.', usuario: usuarioRemovido });
+        } catch (error) {
+            if (error.message === 'Usuário não encontrado') {
+                res.status(404).json( {error: 'Usuário não encontrado.'});
+            } else {
+                res.status(500).json( { error: 'Erro ao deletar usuário.'});
+            }
+        }
+    }
+};
+
+module.exports = UsuáriosController;
