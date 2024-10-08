7f5ef718e096d344b278a8c7711c46ff78551d55
MateusSdeMatos <mateus.sdematos@gmail.com>
2024-10-08 17:14:03 -0300

    Rota Usuários

--- src/routes/usuariosRoute.js
+++ src/routes/usuariosRoute.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const EventosController = require('../controllers/usuariosController');
+const UsuáriosController = require('../controllers/usuariosController');
 
 /**
  * @swagger
  *  components:
  *   schemas:
- *     Evento:
+ *     Usuário:
  *       type: object
  *       required:
  *         - name
@@ -18,7 +18,7 @@ const EventosController = require('../controllers/usuariosController');
  *       properties:
  *         id:
  *           type: string
- *           description: ID único do evento
+ *           description: ID único do usuário
  *         name:
  *           type: string
  *           description: Nome do usuário
@@ -33,8 +33,124 @@ const EventosController = require('../controllers/usuariosController');
  *           description: Senha do usuário
  *         level:
  *           type: string
- *           description:
+ *           description: Nivel de permissão do usuário
  *         status:
  *           type: string
- *           description:   
+ *           description: Status atual do usuário (on/off)
  *       example:
+ *         name: "João Mesquita Arruda"
+ *         email: "João.mesquita@gmail.com"
+ *         user: "João.mesquita"
+ *         password: "Joaozin123" 
+ *         level: "admin"
+ *         status: "on"
+ * 
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Usuários
+ *   description: Gerenciamento de usuários
+ */
+
+/**
+ * @swagger
+ * /api/usuários:
+ *   get:
+ *     summary: Retorna a lista de todos os usuários
+ *     tags: [Usuários]
+ *     responses:
+ *       200:
+ *         description: A lista de usuários
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/'
+ */
+
+router.get('/usuarios', UsuáriosController.getAll);
+
+/**
+ * @swagger
+ * /api/usuários:
+ *   post:
+ *     summary: Cria um novo usuário
+ *     tags: [Usuários]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Usuário'
+ *     responses:
+ *       201:
+ *         description: Usuário criado com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Usuário'
+ *       400:
+ *         description: Dados inválidos
+ */
+router.post('/usuarios', UsuáriosController.create);
+
+/**
+ * @swagger
+ * /api/usuários/{id}:
+ *   put:
+ *     summary: Atualiza um usuário existente
+ *     tags: [Usuários]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: ID do usuário a ser atualizado
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Usuário'
+ *     responses:
+ *       200:
+ *         description: Usuário atualizado com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Usuário'
+ *       404:
+ *         description: Usuário não encontrado
+ */
+router.put('/usuarios/:id', UsuáriosController.update);
+
+/**
+ * @swagger
+ * /api/usuários/{id}:
+ *   delete:
+ *     summary: Deleta um usuário existente
+ *     tags: [Usuários]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: ID do usuário a ser deletado
+ *     responses:
+ *       200:
+ *         description: Usuário deletado com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Usuário'
+ *       404:
+ *         description: Usuário não encontrado
+ */
+router.delete('/usuarios/:id', UsuáriosController.delete);
+
+module.exports = router;
