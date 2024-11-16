79fe36d712cc3459bf4989f6e75b6a3970ce9840
Gabriel Pavan <gabrielpavan2014@outlook.com>
2024-11-16 09:34:38 -0300

    fix add .env

--- src/config/database.js
+++ src/config/database.js
@@ -1,18 +1,17 @@
+require('dotenv').config();
 const { MongoClient } = require('mongodb');
 
-const uri = 'mongodb+srv://admin:bMoxOLdTL4KhtxjY@project2024.gmzv0.mongodb.net/';
-const dbName = 'projeto_gee';
-s
+const { DB_URI, DB_NAME } = process.env
+
 let client;
 let database;
 
 // Função para conectar ao banco de dados
 async function connectToDatabase() {
   if (!client) {
-    client = new MongoClient(uri);
+    client = new MongoClient(DB_URI);
     await client.connect();
-    database = client.db(dbName);
-    console.log("Conectado ao MongoDB");
+    database = client.db(DB_NAME);
   }
   return database;
 }
@@ -23,7 +22,6 @@ async function closeDatabaseConnection() {
     await client.close();
     client = null;
     database = null;
-    console.log("Conexão com MongoDB fechada");
   }
 }
 
