050ed2970e327f6e27b57156cd0c153ce86f8c18
Gabriel Pavan <gabrielpavan2014@outlook.com>
2024-11-30 10:06:50 -0300

    Fix Cards

--- app/app/gesto/dashboard/page.tsx
+++ app/app/gesto/dashboard/page.tsx
@@ -1,56 +1,47 @@
+import { Card, CardContent } from '@/components/ui/card';
 import React from 'react';
 
 const DashboardPage = () => {
   return (
     <div className="min-h-screen pt-4">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
-        {/* Card 1 */}
-        <div className="bg-white p-6 rounded-lg shadow-lg">
-          <h3 className="text-xl font-semibold text-gray-800">Card 1</h3>
-          <p className="text-gray-600 mt-2">Conteúdo do Card 1</p>
-        </div>
-
-        {/* Card 2 */}
-        <div className="bg-white p-6 rounded-lg shadow-lg">
-          <h3 className="text-xl font-semibold text-gray-800">Card 2</h3>
-          <p className="text-gray-600 mt-2">Conteúdo do Card 2</p>
-        </div>
-
-        {/* Card 3 */}
-        <div className="bg-white p-6 rounded-lg shadow-lg">
-          <h3 className="text-xl font-semibold text-gray-800">Card 3</h3>
-          <p className="text-gray-600 mt-2">Conteúdo do Card 3</p>
-        </div>
-
-        {/* Card 4 */}
-        <div className="bg-white p-6 rounded-lg shadow-lg">
-          <h3 className="text-xl font-semibold text-gray-800">Card 4</h3>
-          <p className="text-gray-600 mt-2">Conteúdo do Card 4</p>
-        </div>
-
-        {/* Card 5 */}
-        <div className="bg-white p-6 rounded-lg shadow-lg">
-          <h3 className="text-xl font-semibold text-gray-800">Card 5</h3>
-          <p className="text-gray-600 mt-2">Conteúdo do Card 5</p>
-        </div>
-
-        {/* Card 6 */}
-        <div className="bg-white p-6 rounded-lg shadow-lg">
-          <h3 className="text-xl font-semibold text-gray-800">Card 6</h3>
-          <p className="text-gray-600 mt-2">Conteúdo do Card 6</p>
-        </div>
-
-        {/* Card 7 */}
-        <div className="bg-white p-6 rounded-lg shadow-lg">
-          <h3 className="text-xl font-semibold text-gray-800">Card 7</h3>
-          <p className="text-gray-600 mt-2">Conteúdo do Card 7</p>
-        </div>
-
-        {/* Card 8 */}
-        <div className="bg-white p-6 rounded-lg shadow-lg">
-          <h3 className="text-xl font-semibold text-gray-800">Card 8</h3>
-          <p className="text-gray-600 mt-2">Conteúdo do Card 8</p>
-        </div>
+        <Card>
+          <CardContent className="flex-col pt-3">
+            <h3 className="text-xl font-semibold text-gray-800">Usuarios</h3>
+            <p className="text-gray-600 mt-2">Gerenciar Usuarios</p>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardContent className="flex-col pt-3">
+            <h3 className="text-xl font-semibold text-gray-800">Alunos</h3>
+            <p className="text-gray-600 mt-2">Gerenciar Alunos</p>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardContent className="flex-col pt-3">
+            <h3 className="text-xl font-semibold text-gray-800">Professores</h3>
+            <p className="text-gray-600 mt-2">Gerenciar Professores</p>
+          </CardContent>
+        </Card>
+    
+        <Card>
+          <CardContent className="flex-col pt-3">
+            <h3 className="text-xl font-semibold text-gray-800">Eventos</h3>
+            <p className="text-gray-600 mt-2">Gerenciar Eventos</p>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardContent className="flex-col pt-3">
+            <h3 className="text-xl font-semibold text-gray-800">Profissionais</h3>
+            <p className="text-gray-600 mt-2">Gerenciar Profissionais</p>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardContent className="flex-col pt-3">
+            <h3 className="text-xl font-semibold text-gray-800">Agendamentos</h3>
+            <p className="text-gray-600 mt-2">Gerenciar Agendamentos</p>
+          </CardContent>
+        </Card>
       </div>
     </div>
   );
